5fe5f73103ee832d59f005a41e31ca221c56798c
Shailesh Mangal <shailesh.mangal@getzephyr.com>
2013-05-07 23:32:29 -0700

    Adding selenium ZIP

---
+++ README.md
@@ -0,0 +1,11 @@
+selenium-zip
+============
+
+ZIP (Zephyr Interceptor Program) for Selenium
+
+[Getting Started](https://github.com/zeedeveloper/selenium-zip/wiki "Read Getting Started on wiki page")
+
+[API Docs](http://zeedeveloper.github.com/selenium-zip/apidocs/ "Access javadocs")
+
+~                                                                                                                                                                                                 
+~         
---
+++ src/test/java/com/thed/plugin/SeleniumZephyrInterceptorTest.java
@@ -0,0 +1,34 @@
+package com.thed.plugin;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeleniumZephyrInterceptorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTestcaseExecutionStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestcaseExecutionRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTestcaseExecutionResult() {
+		fail("Not yet implemented");
+	}
+
+}
